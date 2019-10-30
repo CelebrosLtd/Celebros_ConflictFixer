aeb9fb65bab8e696290f9dcb1cebf175da6d1fad
Devbelvg <devbelvg@gmail.com>
2019-10-30 12:42:49 +0000

    0.9.1

---
+++ Block/LayeredNavigation/Navigation/State.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Celebros
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish correct extension functionality.
+ * If you wish to customize it, please contact Celebros.
+ *
+ ******************************************************************************
+ * @category    Celebros
+ * @package     Celebros_ConflictFixer
+ */
+namespace Celebros\ConflictFixer\Block\LayeredNavigation\Navigation;
+
+class State extends \Magento\LayeredNavigation\Block\Navigation\State
+{
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Celebros Conflict Fixer
 
 0.9.0 - conflict with Amasty Layered Navigation module on search page fixed
 
+0.9.1 - conflict with state block and Amasty Shopby module on search page fixed
 
